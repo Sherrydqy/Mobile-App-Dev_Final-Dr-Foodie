755b44c4d144a63f109fe15fc392a90da679db3d
Qinyu <1009908794@qq.com>
2020-04-27 12:44:29 -0400

    Initial Commit

---
+++ Mobile-App-Dev_Final-Dr-Foodie/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Mobile-App-Dev_Final-Dr-Foodie
+//
+//  Created by Qinyu Ding on 4/27/20.
+//  Copyright © 2020 Qinyu. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Mobile-App-Dev_Final-Dr-Foodie/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Mobile-App-Dev_Final-Dr-Foodie
+//
+//  Created by Qinyu Ding on 4/27/20.
+//  Copyright © 2020 Qinyu. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
